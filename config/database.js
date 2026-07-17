1ace45b591b654a9ca8341029d895bee528be942
madangopal <madangopal.redsky@gmail.com>
2026-07-17 19:18:05 +0530

    change db

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = function (dataBaseType) {
 	console.log("process.env.DB_NAME --> ", process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);
 	
 	var sequelize = new dataBaseType(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-		host: process.env.DB_HOST,
+		host: "https://aqw4jp7rcz.preview.c39.airoapp.ai" || process.env.DB_HOST,
 		port: process.env.DB_PORT,
 		dialect: 'mysql',
 		//operatorsAliases: false,
